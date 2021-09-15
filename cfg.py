d06876bf5925c25a93c7629cb9fede73e4837981
jgonzalezdemendibil <67316324+jgonzalezdemendibil@users.noreply.github.com>
2021-09-15 14:09:58 +0200

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -18,7 +18,7 @@ _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 Cfg = EasyDict()
 
-Cfg.use_darknet_cfg = True
+Cfg.use_darknet_cfg = False
 Cfg.cfgfile = os.path.join(_BASE_DIR, 'cfg', 'yolov4.cfg')
 
 Cfg.batch = 64
@@ -54,8 +54,8 @@ Cfg.blur = 0
 Cfg.gaussian = 0
 Cfg.boxes = 60  # box num
 Cfg.TRAIN_EPOCHS = 300
-Cfg.train_label = os.path.join(_BASE_DIR, 'data', 'train.txt')
-Cfg.val_label = os.path.join(_BASE_DIR, 'data' ,'val.txt')
+Cfg.train_label = "/host/Darknet-Pytorch/cfg/train.txt"
+Cfg.val_label = "/host/Darknet-Pytorch/cfg/train.txt"
 Cfg.TRAIN_OPTIMIZER = 'adam'
 '''
 image_path1 x1,y1,x2,y2,id x1,y1,x2,y2,id x1,y1,x2,y2,id ...
